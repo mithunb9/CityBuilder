8181e99934e1edcb5e654541f26504243615eb54
siddarth kotthur <kotthursiddarth@gmail.com>
2022-10-28 11:01:32 -0500

    ping command

--- cogs/Utility.py
+++ cogs/Utility.py
@@ -5,9 +5,9 @@ class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
 
-    @commands.slash_command(name="fuck")
-    async def fuck(self, msg):
-        await msg.respond("fuck u!")
+    @commands.slash_command(name="ping")
+    async def ping(self, msg):
+        await msg.respond("pinged u buddy")
 
 def setup(bot):
     bot.add_cog(Utility(bot))

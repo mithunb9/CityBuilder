7e0ee201f95b42a114e10a2410a7b846056dcac7
Mithun Balasubramanian <sendmithunb@gmail.com>
2022-10-27 23:14:59 -0500

    setup project

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+import discord
+from dotenv import load_dotenv
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+client = MyClient()
+
+load_dotenv()
+client.run(os.getenv('BOT_TOKEN'))
